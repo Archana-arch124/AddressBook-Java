a60aa6baba7064fbbc2faf447ac10cf8a1a00e79
Archana HR <67406339+Archana-arch124@users.noreply.github.com>
2020-12-05 18:03:28 +0530

    added input with pattern conditions

---
+++ AddressBook.java
@@ -0,0 +1,10 @@
+welcome to Addressbook Java probelm
+
+//declaration with pattern
+      static String firstNamePattern = "^[a-zA-Z][a-zA-Z ]*$";
+	    static String lastNamePattern = "^[a-zA-Z][a-zA-Z ]*$";
+	    static String addressPattern = "^[a-zA-Z0-9-,. ]+$";
+	    static String cityPattern = "^[a-zA-Z][a-zA-Z ]*$";
+	    static String statePattern = "^[a-zA-Z][a-zA-Z ]*$";
+	    static String zipPattern = "^\\d{6}$";
+	    static String phoneNumberPattern = "^\\d{10}$";
