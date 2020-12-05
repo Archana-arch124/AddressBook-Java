dad8678ef9aa7af43e408fcfdd4bc3b79d71d944
Archana HR <67406339+Archana-arch124@users.noreply.github.com>
2020-12-05 18:10:39 +0530

    added file create new file

--- AddressBook.java
+++ AddressBook.java
@@ -1,10 +1,49 @@
-welcome to Addressbook Java probelm
-
-//declaration with pattern
-      static String firstNamePattern = "^[a-zA-Z][a-zA-Z ]*$";
+//welcome to Addressbook Java probelm
+package addressbook;
+import java.io.File;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+class AddressBook
+{
+          //declaration with pattern
+            static String firstNamePattern = "^[a-zA-Z][a-zA-Z ]*$";
 	    static String lastNamePattern = "^[a-zA-Z][a-zA-Z ]*$";
 	    static String addressPattern = "^[a-zA-Z0-9-,. ]+$";
 	    static String cityPattern = "^[a-zA-Z][a-zA-Z ]*$";
 	    static String statePattern = "^[a-zA-Z][a-zA-Z ]*$";
 	    static String zipPattern = "^\\d{6}$";
 	    static String phoneNumberPattern = "^\\d{10}$";
+     //scanner variable is declared as static to use throughout the program
+    static final Scanner scanner = new Scanner(System.in);
+    //contains set of contacts which are empty(contact created and no details added)
+    static Set<String> emptyContacts = new HashSet<>();
+    //contains set of contacts which are non empty(contact created and details added)
+    static Set<String> nonEmptyContacts = new HashSet<>();
+
+    // instance methods
+    //creates empty contact
+    void createNewContact() throws Exception 
+    {
+        System.out.print("Enter name of the contact:");
+        String contactName = scanner.nextLine();
+        File file = new File(contactName);
+        if (file.exists()) 
+        {
+            System.out.println("contact " + file.getName() + " already exists!");
+        } 
+        else 
+        {
+            if (file.createNewFile()) 
+            {
+                System.out.println("new contact " + file.getName() + " is created successfully");
+                emptyContacts.add(file.getName());
+            } 
+            else 
+            {
+                System.out.println("file creation failed!");
+            }
+        }
+    }
+}	
+	
